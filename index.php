96265b297130b651656df0e4d580770ec672d737
Adrien Cuisse <alphonse@localhost.localdomain>
2019-07-01 12:32:02 +0200

    Fixed broken twig rendering

--- index.php
+++ index.php
@@ -2,12 +2,17 @@
 
 ini_set('display_errors', 1);
 
+require_once 'vendor/autoload.php';
 require_once 'model/UserModel.class.php';
-require_once 'view/index.htm.twig';
 require_once 'controller/IndexController.class.php';
+
+$loader = new Twig_Loader_Filesystem(__DIR__.'/view');
+$twig = new Twig_Environment($loader);
 
 $user = new UserModel();
 $controller = new IndexController();
+
+$twig->render('index.htm.twig');
 
 if ($controller->trololo())
 {
