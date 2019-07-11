d666bbf14fbb2b31cf96404c216bc54fe35a02dd
Adrien Cuisse <alphonse@localhost.localdomain>
2019-07-11 15:28:15 +0200

    Added working messenger with auto-refresh

--- ajax/messenger_refresh.php
+++ ajax/messenger_refresh.php
@@ -2,16 +2,38 @@
 
 ini_set('display_errors', 1);
 
+require_once '../vendor/autoload.php';
 require_once '../model/MessageModel.class.php';
 
-$newmessages = new MessageModel();
+/*$newmessages = new MessageModel();
 
 //var_dump($_POST);
-var_dump(
+/*var_dump(
   $newmessages->new_content_conv(
     (int) $_POST['id_conv'],
     (int) $_POST['id_msg']
   )
+);*/
+
+//var_dump($_POST);
+
+$messenger = new MessageModel();
+$twig = new Twig_Environment(
+  new Twig_Loader_Filesystem(__DIR__ . '/../view')
 );
+
+$latest_messages = $messenger->fetch_messages(
+  $_POST['conv-id'],
+  $_POST['timestamp']
+);
+foreach ($latest_messages as $message)
+{
+  echo $twig->render('messageBubble.htm.twig', [
+    'username' => $message['username'],
+    'content' => $message['content'],
+    'id' => $message['id'],
+    'account_name' => $_POST['account_name']
+  ]);
+}
 
 ?>
--- js/messenger.js
+++ js/messenger.js
@@ -1,4 +1,4 @@
-const trololo = (response) =>
+/*const trololo = (response) =>
 {
   console.log(response);
 };
@@ -56,3 +56,80 @@ $(document).ready(function()
 
 // après envoi d'un message -> auto refresh
 // au bout de quelques secondes -> auto refresh
+*/
+
+/*
+** The timestamp after which messages have to be fetched
+*/
+let timestamp = Date.now();
+
+/*
+** Gets all new messages from the server and adds them to the display zone
+*/
+const fetchMessages = () =>
+{
+  let profileLink = $('.nav-link[href^="profile.php"]').attr('href'); 
+  let account_name = profileLink.substr(profileLink.lastIndexOf('=') + 1);
+
+  $.post(
+    'ajax/messenger_refresh.php',
+    {
+      timestamp: timestamp,
+      'conv-id': urlParams().id,
+      account_name: account_name
+    },
+    displayMessages,
+    'html'
+  );
+
+  timestamp = Date.now();
+};
+
+/*
+** Callback for fetchMessages()
+** Adds messages to the display zone
+**
+** @var htmlResponse: messages as sent by the ajax request
+*/
+const displayMessages = (htmlResponse) =>
+{
+  $(htmlResponse).insertBefore('#messenger-controls');
+};
+
+/*
+** Callback for send button
+** Checks the message form, then submit it if there's no errors, and updates the display zone
+*/
+$('#messenger-send').click((event) =>
+{
+  event.preventDefault();
+  
+  $.post(
+      'ajax/message_send.php',
+      {
+          'messenger-input': $('#messenger-input').val()
+      },
+      treatAjaxFormResponse,
+      'json'
+  ).then(() =>
+  {
+    if ($('#messenger-form').find('.invalid-feedback').length === 0)
+    {
+      $.post(
+        currentUrl(),
+        {
+          'messenger-input': $('#messenger-input').val()
+        }
+      ).then(() =>
+      {
+        fetchMessages();
+        $('#messenger-input').val('');
+      });
+    }
+  });
+});
+
+/*
+** Autorefresh
+*/
+setInterval(fetchMessages, 3000);
